e0a5410f7a364e59461a8b75e778a8c9dd51e834
Rodrigo <Rodrigo@HP_RODRIGO>
2023-10-04 21:25:15 -0500

    Agregar archivos de proyecto.

---
+++ lab07_04/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab07_04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int lineas;
+            Console.Write("Ingrese el número de lineas: ");
+            lineas = int.Parse(Console.ReadLine()); 
+             for (int i = 0; i < lineas; i++)
+            {
+                int numero;
+                Console.Write("Ingrese un número: ");
+                numero = int.Parse(Console.ReadLine()); 
+                for (int  j = 0; j < numero; j++)
+                {
+                    for (int r = 0; r < numero*2; r++)
+                    {
+                        Console.Write("*");
+                    }
+                    Console.WriteLine();
+                }
+            }
+             Console.ReadKey();
+        }
+    }
+}
